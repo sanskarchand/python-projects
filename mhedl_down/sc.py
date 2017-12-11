514b23b704fc22cd362a00cf79a059bc80f80470
drakinosh <sanskarchand@gmail.com>
2017-12-11 15:19:56 +0545

    fixed volume numbers

--- mhedl_down/sc.py
+++ mhedl_down/sc.py
@@ -5,9 +5,21 @@ import bs4
 import re
 
 ERR_STRING = "CHAPTER_ERROR"
-#chap_pat = re.compile("/c\S*/")
-# pattern: /c, followed by digits or periods or digits
+
+#values
+CHAP_FORM = 3
+VOL_CHAP_FORM = 4
+
+FORMAT = 0
+DEBUG = True
+
+VOL_WEIGHT = 1000
+INF = 88888888
+
+#pattern: /c, followed by digits or periods or digits
 chap_pat = re.compile("/c\d*.*\d*/")
+#pattern: /v, followed by digits
+vol_pat = re.compile("/v\d*/")
 
 manga_name = sys.argv[1]
 manga_foldername = sys.argv[2]
@@ -17,30 +29,56 @@ mp_url_base = "https://www.mangahere.co/manga/"
 mp_url = mp_url_base + manga_name
 mp_fname = manga_name + ".html"
 
-DEBUG = True 
+
+def setChapterFormat(chap_string):
+    """
+    If the link has the pattern /vxxx/, we assume that the manga
+    is split into volumes. If so, we need to add the volume name to 
+    the folder.
+    This sets FORMAT according to the format of the string.
+    NOTE: It is not necessary to check this every chapter
+    """
+    global FORMAT
+    vol_check = vol_pat.search(chap_string)
+
+    if vol_check is None:
+        FORMAT = CHAP_FORM
+    else:
+        FORMAT = VOL_CHAP_FORM
 
 def extractChapterCode(chap_string):
-    if DEBUG:
-        print("Given str: ", chap_string)
-
     try:
-        x = chap_pat.search(chap_string) 
-        st = x.group() # form /c[num]/
-        return st[1:-1] #return c[num]
+        x = chap_pat.search(chap_string)
+        st = x.group()
+        if (DEBUG): print("CODE: ", st[1:-1])
+        return st[1:-1] # return excluding bounding /'s
     except:
         return ERR_STRING
 
+def extractVolumeCode(chap_string):
+    try:
+        x = vol_pat.search(chap_string)
+        st = x.group()
+        return st[1:-1] # reuturn v[num]
+    except:
+        return ERR_STRING
 
 def downloadSingleChapter(c_link):
     
-    # phase one : make dir
-    
-    i = c_link.index(manga_name)
-    # get the c[num] part, excluding the bounding /'s
-    #foldername = c_link[i + len(manga_name) + 1:-1]
-    #foldername = c_link[-5:-1]
-    foldername = extractChapterCode(c_link)
+    # phase one : check format and make dir
+    setChapterFormat(c_link)
 
+    if DEBUG:
+        print("FORMAT: ", FORMAT)
+
+    chap_code = extractChapterCode(c_link)
+    vol_code = ''
+
+    if (FORMAT == VOL_CHAP_FORM):
+        vol_code = extractVolumeCode(c_link) + '_'
+
+    foldername = vol_code + chap_code
+    
     if (foldername == ERR_STRING):
         return
 
@@ -128,10 +166,24 @@ def sortChapKey(chap):
     if ccode is not ERR_STRING:
         return float(ccode[1:])
     else:
-        return 99999
+        return INF
+
+def sortVolChapKey(chap):
+    """
+    number from vol
+    """
+
+    volcode = extractVolumeCode(chap)
+    ccode = extractChapterCode(chap)
+    if volcode is not ERR_STRING and ccode is not ERR_STRING:
+        return VOL_WEIGHT * float(volcode[1:]) + float(ccode[1:])
+    else:
+        return INF
 
 def main():
     
+    global FORMAT
+
     # make dir
     try:
         os.mkdir(manga_foldername)
@@ -140,7 +192,10 @@ def main():
 
     # Download main page
     print("MP-URL: ", mp_url)
-    r = requests.get(mp_url, verify=False, headers={'Accept-Encoding':'deflate'})
+    
+    #head = {'Accept-Encoding':'deflate', 'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:57.0) Gecko/20100101 Firefox/57.0'}
+    head = {'Accept-Encoding':'deflate'}
+    r = requests.get(mp_url, verify=False, headers=head)
     assert r.status_code == 200
 
     with open(mp_fname, 'wb') as f:
@@ -166,8 +221,15 @@ def main():
 
                 chap_li.append(li)
 
-    #chap_li.reverse()
-    chap_li.sort(key=sortChapKey) # Sorts by chapter number
+
+    # set format acc to tha of the first chap
+    setChapterFormat(chap_li[0])
+
+    # sort by chapter
+    if FORMAT ==  VOL_CHAP_FORM: # FORMAT OF LAST
+        chap_li.sort(key=sortVolChapKey) # Sorts by chapter number
+    else:
+        chap_li.sort(key=sortChapKey)
 
     print('Chapter list: ')
     for idx, each in enumerate(chap_li):
@@ -176,7 +238,7 @@ def main():
     print('Total number of chapters: {}'.format(len(chap_li)))
 
     dl_start = int(input(">>Enter DL start list index: "))
-    dl_stop = int(input(">>Enter DL stop list indexX: "))
+    dl_stop = int(input(">>Enter DL stop list index: "))
 
     downloadChapters(chap_li, dl_start, dl_stop)
 
